3701191ba8127a13e5c87d22d62f1e2062596183
GavrilBogdan <gavrilbogdan3@gmail.com>
2025-11-14 22:05:54 +0200

    abc

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,17 +1,14 @@
-<<<<<<< Updated upstream
 import Hero from "@/components/Hero";
-
-export default function Home() {
-  return (
-    <>
-      <Hero />
-    </>
-=======
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-   <Navbar/>
->>>>>>> Stashed changes
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Hero />
+      </main>
+    </>
   );
 }
